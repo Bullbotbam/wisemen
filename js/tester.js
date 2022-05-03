f77454364b6de373a2ad4a16b89815d02d4b01ac
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2022-05-03 10:25:26 -0500

    Update tester.js

--- js/tester.js
+++ js/tester.js
@@ -1,26 +1,49 @@
-let trevor = new Array();
-let maxArr = 100;
-let maxTrevie = 10000;
+function divisors(integer) {
+	let divS = [];
 
-for (let i = 2; i < maxTrevie; i++) trevor[i] = 1;
+	for (let i = 2; i < integer; i++) {
+		if (integer % i === 0) {
+			divS.push(i);
+		}
+	}
 
-// Using the trevor to make the primes
-let primes = [];
-let sum = 0;
-for (let prime = 2; prime < maxTrevie && primes.length < maxArr; prime++) {
-	if (!trevor[prime]) continue;
-	primes.push(prime);
-	sum += prime;
-	for (let i = prime * 2; i < maxTrevie; i += prime) trevor[i] = 0;
+	return divS.length ? divS : `${integer} is prime`;
 }
 
-document.getElementById('result').value =
-	'primes: ' +
-	primes.join(' ') +
-	'\n' +
-	'count: ' +
-	primes.length +
-	'\n' +
-	'sum: ' +
-	sum +
-	'\n';
+// function divisors(integer) {
+// 	let divisors = [];
+
+// 	for (let i = 2; i < integer; i++) {
+// 		if (integer % i === 0) {
+// 			divisors.push(i);
+// 		}
+// 	}
+// 	return divisors.length ? divisors : `${integer} is prime.`;
+// }
+
+// let trevor = new Array();
+// let maxArr = 100;
+// let maxTrevie = 10000;
+
+// for (let i = 2; i < maxTrevie; i++) trevor[i] = 1;
+
+// // Using the trevor to make the primes
+// let primes = [];
+// let sum = 0;
+// for (let prime = 2; prime < maxTrevie && primes.length < maxArr; prime++) {
+// 	if (!trevor[prime]) continue;
+// 	primes.push(prime);
+// 	sum += prime;
+// 	for (let i = prime * 2; i < maxTrevie; i += prime) trevor[i] = 0;
+// }
+
+// document.getElementById('result').value =
+// 	'primes: ' +
+// 	primes.join(' ') +
+// 	'\n' +
+// 	'count: ' +
+// 	primes.length +
+// 	'\n' +
+// 	'sum: ' +
+// 	sum +
+// 	'\n';
